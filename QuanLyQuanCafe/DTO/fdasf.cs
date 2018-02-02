e17e509ac57f158b08bff7c95cf4ca3c092d8584
NguyenVanThuong <robinvandante@gmail.com>
2018-02-02 14:22:53 +0700

    Dataa

--- QuanLyQuanCafe/DTO/fdasf.cs
+++ QuanLyQuanCafe/DTO/fdasf.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLyQuanCafe.DAO
+namespace QuanLyQuanCafe.DTO
 {
-    class DataProvider
+    class fdasf
     {
-        fdsajfkl
     }
 }
